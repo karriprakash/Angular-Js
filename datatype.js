26d02c2f1d2980f2bdbd55ef68034d2abb05533f
Karri Surya Venkata Prakash Rao <karriprakash342@gmail.com>
2021-12-25 01:03:32 +0530

    Second Commit

---
+++ datatype.js
@@ -0,0 +1,26 @@
+//String
+var firstname = "Sachin";
+console.log(firstname);
+//Number
+var num;
+num = 24;
+console.log(num);
+//Boolean
+var isMarried = false;
+console.log(isMarried);
+//array dataType
+//Number Array
+var ar1 = [1, 2, 3, 4];
+console.log(ar1);
+//--------------
+//String Array
+var ar2 = ["Sachin", "Virat"];
+console.log(ar2);
+//-------------
+//Boolean Array
+var ar3 = [false, true];
+console.log(ar3);
+//-------------
+//Hetrogenous Type of Data
+var ar4 = [1, "Sachin", true];
+console.log(ar4);
---
+++ datatype.ts
@@ -0,0 +1,26 @@
+//String
+var firstname : String = "Sachin";
+console.log(firstname)
+//Number
+var num:Number
+num = 24
+console.log(num)
+//Boolean
+var isMarried:Boolean = false
+console.log(isMarried)
+//array dataType
+//Number Array
+var ar1:Number[] = [1,2,3,4]
+console.log(ar1)
+//--------------
+//String Array
+var ar2:String[] = ["Sachin","Virat"]
+console.log(ar2)
+//-------------
+//Boolean Array
+var ar3:Boolean[] = [false,true]
+console.log(ar3)
+//-------------
+//Hetrogenous Type of Data
+var ar4:any[] = [1,"Sachin",true]
+console.log(ar4)
